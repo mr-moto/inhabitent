4ac5182f47cd8cbf54e015682761ad03a49ce49c
mr-moto <khash615@gmail.com>
2017-02-19 14:23:08 -0800

    styled blog post featured images, added sidebar content

---
+++ themes/inhabitent/about.php
@@ -0,0 +1,20 @@
+
+<?php /* Template Name: About */ ?>
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+get_header(); ?>
+    <section class="about-hero">
+        <h1><?php single_post_title(); ?></h1>
+    </section>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
--- themes/inhabitent/sidebar.php
+++ themes/inhabitent/sidebar.php
@@ -12,4 +12,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	
 </div><!-- #secondary -->
