1d7fa588a791c47aabbfbc0a303e772e332c1d15
mr-moto <khash615@gmail.com>
2017-02-24 13:15:25 -0800

    added journal single page

--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -13,11 +13,21 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<div class='social'>
+              <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a>
+              <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a>
+              <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a>
+            </div>
+			<?php
+// If comments are open or we have at least one comment, load up the comment template.
+if ( comments_open() || get_comments_number() ) :
+    comments_template();
+endif;
+?>
 			
 		<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
+		</main><!-- #mainnnnm -->
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
