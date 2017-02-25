ca2d2f64585bf4ead8c00bee80048ac7b29bebf9
mr-moto <khash615@gmail.com>
2017-02-25 15:54:21 -0800

    styled sidebar

--- plugins/business-hours-widget/views/widget.php
+++ plugins/business-hours-widget/views/widget.php
@@ -3,7 +3,7 @@
 
 <?php if ( strlen(trim( $monday_friday ) ) > 0 ) : ?>
     <p>
-        <span class='day-of-week'>Monday to Friday:</span> <?php echo $monday_friday ?> 
+        <span class='day-of-week'>Monday-Friday:</span> <?php echo $monday_friday ?> 
     </p>
 <?php endif; ?>
 
